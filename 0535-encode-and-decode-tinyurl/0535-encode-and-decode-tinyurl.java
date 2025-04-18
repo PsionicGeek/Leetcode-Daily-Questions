5f99e10f14f59a80abe8cb8b55fff2ab6074d129
Ravi Kumar Singh <64123658+PsionicGeek@users.noreply.github.com>
2025-04-18 15:13:50 +0530

    Update 0535-encode-and-decode-tinyurl.java

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,24 +1,24 @@
 public class Codec {
+    Map<String, String> codeDB = new HashMap<>(), urlDB = new HashMap<>();
+    static final String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    // Encodes a URL to a shortened URL.
+    private String getCode() {
+        char[] code = new char[6];
+        for (int i = 0; i < 6; i++) 
+            code[i] = chars.charAt((int)(Math.random() * 62));
+        return "http://tinyurl.com/" + String.valueOf(code);
+    }
+    
     public String encode(String longUrl) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : longUrl.toCharArray()) {
-            sb.append((char)(c + 1)); // Shift each character by +1
-        }
-        return sb.toString();
+        if (urlDB.containsKey(longUrl)) return urlDB.get(longUrl);
+        String code = getCode();
+        while (codeDB.containsKey(code)) code = getCode();
+        codeDB.put(code, longUrl);
+        urlDB.put(longUrl, code);
+        return code;
     }
 
-    // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : shortUrl.toCharArray()) {
-            sb.append((char)(c - 1)); // Reverse the shift
-        }
-        return sb.toString();
+        return codeDB.get(shortUrl);
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.decode(codec.encode(url));
